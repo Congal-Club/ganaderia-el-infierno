9679e7cc7993e9ef7cc3a8ded1cc1675b221a8bb
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-10-30 19:12:16 -0600

    Add initial products section

---
+++ src/types/products.ts
@@ -0,0 +1,19 @@
+export interface Product {
+  id:                  number
+  model:               string
+  slug:                string
+  stock:               number
+  image:               string
+  images:              string[]
+  price:               number
+  weight:              number
+  length:              number
+  width:               number
+  height:              number
+  status:              string
+  description:         string
+  "short-description": string
+  name:                string
+  tag:                 string[]
+  categories:          string[]
+}
