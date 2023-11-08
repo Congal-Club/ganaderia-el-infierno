6d4c7494422b89f671ed25f6281b6c2d3c5653e1
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-11-08 11:46:40 -0600

    Fix google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-
+import partytown from '@astrojs/partytown';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown()],
   output: "server",
   adapter: vercel()
 });
