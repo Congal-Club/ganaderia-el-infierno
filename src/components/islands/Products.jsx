8b4eb8cd3a8c01fe065200ddeb0376603f6980d9
Hector <pipenmagic13@gmail.com>
2023-11-30 18:35:05 -0600

    Nav and filtered of products

--- src/components/islands/Products.jsx
+++ src/components/islands/Products.jsx
@@ -1,32 +1,74 @@
-import { Product } from '../../types/products'
-import { products } from '../../data/db'
+import Product from "./Product";
+import {useState} from "react"
+export default function Products({ products }) {
+  const [filteredProducts, setFilteredProducts] = useState(products)
 
-export default function Products(){
-  
-  return(
+  const handleChange = (e) =>{
+    const value = e.target.value
+    switch(value){
+      case 'A-Z':{
+        setFilteredProducts(
+          filteredProducts
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map(p => p)
+          )
+          break
+      }
+      case 'Z-A':{
+        setFilteredProducts(
+          filteredProducts
+            .sort((a, b) => b.name.localeCompare(a.name))
+            .map(p => p)
+          )
+          break
+      }
+      case 'HtoL':{
+        setFilteredProducts(
+          filteredProducts
+            .sort((a, b) => b.price - a.price)
+            .map(p => p)
+          )
+          break
+      }
+      case 'LtoH':{
+        setFilteredProducts(
+          filteredProducts
+            .sort((a, b) => a.price - b.price)
+            .map(p => p)
+          )
+          break
+      }
+    }
+  }
+  return (
     <section>
-      <h2 class="text-2xl font-semibold text-slate-700 mb-6">
+      <h2 className="text-2xl font-semibold text-slate-700 mb-6">
         Productos destacados
       </h2>
-      <div class="mb-4">
-        <label>Ordenar por</label>
-        <select name="filterMenu" id="filterMenu">
-          <option>Seleccionar...</option>
-          <option value="Alphabet Asc">Alfabeticamente de manera ascendente</option>
-          <option value="Alphabet Desc">Alfabeticamente de manera descendente</option>
-          <option value="Price H-L">Precio alto-bajo</option>
-          <option value="Price L-H">Precio bajo-alto</option>
+      <div className="mb-4 flex justify-start items-center gap-2">
+        <label className="font-medium">Ordenar por</label>
+        <select className="py-2 px-4 border border-gray-300 rounded-md" name="filterMenu" id="filterMenu" onChange={handleChange}>
+          <option value='default'>Seleccionar...</option>
+          <option value="A-Z">
+            Alfabeticamente de manera ascendente
+          </option>
+          <option value="Z-A">
+            Alfabeticamente de manera descendente
+          </option>
+          <option value="HtoL">
+            Precio alto-bajo
+          </option>
+          <option value="LtoH">
+            Precio bajo-alto
+          </option>
         </select>
       </div>
-      {products.map((product) =>(
-        <p key={product.id}>{product.name}</p>
-      ))}
 
-
-      <section class="w-full flex justify-start items-start gap-6 flex-wrap">
-      
-    
-  </section>
+      <section className="w-full flex justify-start items-start gap-6 flex-wrap">
+        {filteredProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </section>
     </section>
-  )
+  );
 }
