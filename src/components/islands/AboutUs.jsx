46f1e2275d664d0906716b9d0368a509b371cfd8
Hector <pipenmagic13@gmail.com>
2023-12-04 16:18:37 -0600

    about page

---
+++ src/components/islands/AboutUs.jsx
@@ -0,0 +1,48 @@
+export default function About(){
+  return(
+    <div>
+      <h1 class="text-3xl mb-5"><b>Acerca de</b></h1>
+      <div class="text-justify text-lg">
+
+        <p>
+        En Ganaderia el infierno, nos dedicamos con pasión y compromiso a brindar soluciones integrales para el sector ganadero. 
+        Con años de experiencia en la industria, nos enorgullece ser tu socio confiable en la adquisición de ganado de calidad 
+        y suministros nutricionales especializados.
+        </p>
+        <h2 class="mt-8"><b>¿Quiénes somos?</b></h2>
+        <p>
+        Somos más que un negocio; somos amantes apasionados de la ganadería, comprometidos con la excelencia y el bienestar animal. 
+        Nuestra misión es proporcionar a nuestros clientes ganado saludable y productos de alimentación de alta calidad que impulsen 
+        el rendimiento y la prosperidad en sus operaciones.
+        </p>
+        <h2 class="mt-8"><b>Lo Que Ofrecemos</b></h2>
+        <ul class="list-disc ml-14">
+          <li>
+            <span class="font-bold">Ganado de Calidad:</span>  Seleccionamos cuidadosamente cada animal, garantizando la salud, genética y calidad que cumplen con los
+            estándares más exigentes.
+          </li>
+          <li>
+            <span class="font-bold">Alimentación Especializada:</span>  Ofrecemos una gama completa de alimentos balanceados y suplementos nutricionales para garantizar 
+            el óptimo desarrollo y rendimiento de tu ganado.
+          </li>
+          <li>
+            <span class="font-bold">Asesoramiento Expert:</span>  Nuestro equipo de expertos está aquí para proporcionarte asesoramiento personalizado y respaldar tus 
+            decisiones en la gestión del ganado y la alimentación.
+          </li>
+        </ul>
+        <h2 class="mt-8"><b>Compromiso con la Calidad y el Cliente:</b></h2>
+        <p>
+          En Ganaderia el Infierno, la calidad y la satisfacción del cliente son nuestra prioridad. Trabajamos arduamente para construir 
+          relaciones sólidas y duraderas, ofreciendo productos y servicios que superan las expectativas.
+        </p>
+        <p class="mt-5">
+        Gracias por elegir Ganaderia el Infierno. Estamos emocionados de ser parte de tu éxito en la ganadería.
+        </p>
+        <p class="mt-3 text-center ">
+          ¡Descubre la diferencia con <a href="/"> <span class="text-red-700 font-bold ">Ganaderia el Infierno</span> </a>  !
+        </p>
+      </div>
+
+    </div>
+  )
+}
