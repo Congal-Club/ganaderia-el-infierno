a1636ae84d4b4337607479d840b2d635dc319e6b
Hector <pipenmagic13@gmail.com>
2023-12-04 16:17:49 -0600

    slider to product

--- src/components/islands/Slider.jsx
+++ src/components/islands/Slider.jsx
@@ -22,46 +22,54 @@ export default function Slider() {
       >
         <SwiperSlide className="bg-red-500">
           <div>
-            <img
-              src="/images/slider1.png"
-              className="w-full h-[600px] object-cover object-center"
-              alt="Toro charolais de alta calidad"
-              title="Toro charolais de alta calidad"
-              srcSet=""
-            />
+            <a title="Toro charolais de alta calidad" href="/products/toro-charolais-alta-calidad">
+              <img
+                src="/images/slider1.png"
+                className="w-full h-[600px] object-cover object-center"
+                alt="Toro charolais de alta calidad"
+                title="Toro charolais de alta calidad"
+                srcSet=""
+              />
+            </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
-            <img
-              src="/images/slider2.png"
-              className="w-full h-[600px] object-cover object-center"
-              alt="Toro angus de alta calidad"
-              title="Toro angus de alta calidad"
-              srcSet=""
-            />
+            <a href="/products/vaca-holstein-leche">
+              <img
+                src="/images/slider2.png"
+                className="w-full h-[600px] object-cover object-center"
+                alt="Toro angus de alta calidad"
+                title="Toro angus de alta calidad"
+                srcSet=""
+              />
+            </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
-            <img
-              src="/images/slider3.png"
-              className="w-full h-[600px] object-cover object-center"
-              alt="Becerro hereford"
-              title="Becerro hereford"
-              srcSet=""
-            />
+            <a href="/products/becerro-simmental-registro">
+              <img
+                src="/images/slider3.png"
+                className="w-full h-[600px] object-cover object-center"
+                alt="Becerro hereford"
+                title="Becerro hereford"
+                srcSet=""
+              />
+            </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
-            <img
-              src="/images/slider4.png"
-              className="w-full h-[600px] object-cover object-center"
-              alt="Toro angus negro"
-              title="Toro angus negro"
-              srcSet=""
-            />
+            <a href="/products/toro-angus-negro">
+              <img
+                src="/images/slider4.png"
+                className="w-full h-[600px] object-cover object-center"
+                alt="Toro angus negro"
+                title="Toro angus negro"
+                srcSet=""
+              />
+            </a>
           </div>
         </SwiperSlide>
       </Swiper>
